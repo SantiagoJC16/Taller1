6aae390047b7004ef58cfa46ad70a4f21919ddd7
User <User@DESKTOP-NPORBR9>
2025-04-26 15:37:43 -0500

    para el taller

---
+++ src/schaflatallergit/SChaflaMateria.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package schaflatallergit;
+
+/**
+ *
+ * @author User
+ */
+public class SChaflaMateria {
+    public static void main(String[] args) {
+     int a = 5;
+        int b = 7;
+        int suma = a + b;
+        System.out.println("La suma de " + a + " y " + b + " es: " + suma);
+    
+}}
